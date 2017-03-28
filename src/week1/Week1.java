1f6015f95dfb9216fa0b359b30c35242f9c4984b
notroot <notroot@email.server>
2017-03-27 19:42:26 -0700

    upload

---
+++ src/week1/Week1.java
@@ -0,0 +1,9 @@
+package week1;
+
+public class Week1
+{
+	public static void main(String[] args)
+	{
+		System.out.println("JavaIII - Week1");
+	}
+}
---
+++ src/week2/Week2.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class Week2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("JavaIII - Week2");
+	}
+}
