b9133a39ba0e2ea35c48fd125dd0ede9c51679fc
notroot <notroot@email.server>
2017-04-22 18:15:14 -0700

    upload

---
+++ src/assgn/add/InvMainApp.java
@@ -0,0 +1,28 @@
+package assgn.add;
+
+
+
+public class InvMainApp
+{
+	public static void main (String[] args)
+	{
+		
+		InvView theView = new InvView();
+		InvModel theModel = new InvModel();
+		InvController theController = new InvController(theView, theModel);
+		theView.setVisible(true);
+		
+		/*
+		theModel.setTitle("SoundsOfJava");
+		theModel.setProdID("Jav-1234");
+		
+		System.out.println("===========================================");
+		System.out.println("=============Results==================\n\n");
+		System.out.println(theModel.getTitle());
+		System.out.println(theModel.getProdID());
+		*/
+		
+		
+
+	}
+}
