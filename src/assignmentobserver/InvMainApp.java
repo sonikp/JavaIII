aaf673b2dbca4589d05e2f2fd1319d523011035d
notroot <notroot@email.server>
2017-04-30 17:57:31 -0700

    upload

--- src/assignmentobserver/InvMainApp.java
+++ src/assignmentobserver/InvMainApp.java
@@ -1,18 +1,48 @@
 package assignmentobserver;
 
+/**
+ * The Inventory Application 
+ * This is an implementation of the MVC design concept for implementing an Inventory
+ * application as a JavaIII Project. Additionally this required to use the Observer 
+ * design pattern.
+ * Inventory System:
+ * 	- manage an inventory comprised of CDs, DVDs, and books 
+ * 	- enter new items, search for and edit existing items, and delete items
+ * 		- Create, Retrieve, Update, and Delete
+ * 	- input/output (System.in / System.out)
+ * 		- Use scanner class
+ * 	- User interface modularized as a View (or Delegate) component
+ * 		- initially text based
+ * 	- inventory representation will be modularized as a Model
+ * 		- will be persistent
+ * 		- will be maintained using the Properties class
+ * 	- Will make use of: 
+ * 		- inheritance - individual items CD, DVD, BOOK, inherit from the inventory class 
+ * 		- polymorphism - InvPersistance extends Properties.class, (polymorphism) 
+ * 		- interface definition
+ * 	- make use of at least one enumeration (enum type) 
+ * 
+ * 
+ * @author Michael Floerchinger
+ *
+ */
 
-
+// https://www.youtube.com/watch?v=Lsdaztp3_lw
 
 public class InvMainApp
 {
 	public static void main (String[] args) throws Exception
 	{
-		// create objects		
+		/**
+		 * Creates the MVC Objects
+		 */		
 		InvModel theModel = new InvModel();
 		InvController theController = new InvController();	
 		InvView theView = new InvView();	
 		
-		// link the objects
+		/**
+		 * Connects the objects
+		 */
 		theController.setTheView(theView);
 		theController.setTheModel(theModel);
 		
@@ -22,7 +52,9 @@ public class InvMainApp
 		theView.setTheModel(theModel);
 		theView.setTheController(theController);
 		
-		// start application
+		/**
+		 * Starts the application in the View component
+		 */
 		theView.start();
 
 
