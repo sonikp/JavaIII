167eea69c8b112ba2e713a7d48a83792ed65a6de
notroot <notroot@email.server>
2017-06-05 19:56:21 -0700

    update

--- src/week7/Week7Notes.java
+++ src/week7/Week7Notes.java
@@ -79,6 +79,76 @@ Additional reading:
 http://www.javacreed.com/understanding-threads-monitors-and-locks/
 
 
+==========Final weeks notes=================
+chapter 23:
+
+Class SwingWorker definitions:
+
+do In Background - Crunch Stuff!! (Worker Thread)
+done - wrap up code (event dispatch thread)
+execute - schedule swing worker object (to execute in worker thread)
+get - wait for finished computation
+publish - sends partial/intermediate results to "process" method
+process - receives partial/intermediate results from "publish" method and processes those results
+setProgress - set progress property
+ 
+fig 23.20 
+
+synchronized (keyword) =  (multi-threading)
+- is the key word to ensure thread safety.
+- ensures only one thread at a time can safely get into the method 
+- makes use of a monitor lock. (All java objects have a monitor lock)
+- the lock is given to that thread.
+- lock operates on the entire object, only with respect to synchronized methods
+
+examples:
+synchronized(this) {
+
+}
+synchronized(new Object) {
+
+}
+
+- All objects can use monitor locks
+ Another class that can be useful with similar functions is the swingworker class
+ SwingWorkerClass Chapter 23.11
+ 
+ fig 23.24
+ 
+ fig 23.26
+ 
+ SwingWorker < Integer, Integer > (second item is an intermediary type. When this is not used, use the "Object" tag)
+
+fig 23.18 circular buffers
+ 
+fig 23.20 reentrantlock = allows for locking of an object, however when a method calls a locked method, that is part of the thread that has caused the lock, then the locked thread 
+locks that locked method in order to complete and let the original locking thread to complete
+
+Synchronized keyword = locks and unlocks within a designated areas of code within the area that is designated as "locked". 
+
+Concurrent locks =  allows for locking and unlocking of methods, by specifying .unlock() at any time you need. this means that
+it is not necessary to lock and unlock within the same section of code.
+
+?? figuring out errors and debuging code problems
+Tool: profiler =  allows you to get metrics on the executed application. 
+Step through the debugger
+
+Chapter 23.24 - Interfaces Callable and Future objects
+
+MVC is one example of a set of architectural structures and design patterns 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 */
